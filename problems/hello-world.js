05ffedc835022411f5c7b173541d21149cd43485
Jonathan Bedon <bedonjonathan@gmail.com>
2020-12-12 12:47:02 -0500

    Pass text

--- problems/hello-world.js
+++ problems/hello-world.js
@@ -4,6 +4,7 @@
  * @returns undefined
  */
 function helloWorld() {
+  console.log("hello world")
 
 }
 
@@ -14,8 +15,8 @@ function helloWorld() {
  * @param {string} name - the name to be added to the console log 
  * @returns undefined
  */
-function helloName() {
-
+function helloName( name ) {
+  console.log( "hello " + name);
 }
 
 /**
@@ -23,8 +24,9 @@ function helloName() {
  * 
  * @returns {string} - the string "hello world"
  */
-function returnHelloWorld() {
-
+function returnHelloWorld(hello) {
+  hello = "hello world"
+return hello
 }
 
 /**
@@ -34,8 +36,9 @@ function returnHelloWorld() {
  * @param {string} name - the name to added to the return string
  * @returns {string} A greeting in the form of "Hello ${name}!"
  */
-function returnHelloName() {
-
+function returnHelloName( name ) {
+  name = ( "Hello " + name + "!")
+  return name 
 }
 
 module.exports = {
